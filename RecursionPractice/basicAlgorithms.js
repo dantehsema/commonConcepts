f46d9cfb9c7fdab34f5db6d7b4e0dad9cc19de20
Sema <Danteh.Sema@baltimorecity.gov>
2019-08-15 20:59:35 -0400

    basicAlgorithms

---
+++ RecursionPractice/basicAlgorithms.js
@@ -0,0 +1,143 @@
+let reversed;
+function tryingReversals(str){
+	let strSplit = str.split('');
+	console.log('here is the string split:', strSplit);
+	let strReverse = strSplit.reverse();
+	console.log('here is string in array as its reversed:', strReverse);
+	let strJoin = strReverse.join('');
+	return strJoin;
+}
+
+
+function reverseString(str){
+	let newStr = str.split('').reverse().join('');
+	return newStr;
+}
+
+
+function findLongestWordLength(str) {
+	let stringSplit = str.split(' ');
+	let lengths = [];
+	console.log('Here is the string in ArrayForm:',stringSplit);
+	for(let i = 0; i<stringSplit.length; i++){
+		lengths.push(stringSplit[i].length);
+		console.log('lengths in a string:', lengths);
+	}
+
+  return Math.max(...lengths);
+
+}
+
+
+function largestOfFour(arr) {
+  // You can do this!
+  let arrFlat = arr.flat();
+ // console.log('Here is your flat arr:',arrFlat);
+  let arrMax = Math.max(...arrFlat);
+ // console.log('Her is the maximum number in ArrFlat:', arrMax);
+  for(let i=0; i<arr.length; i++){
+  //		console.log('Is my for Loop working:',arr[i])
+  	if(arr[i].indexOf(arrMax) >= 0){
+  //		console.log('here is the array with the maximum #:', arr[i]);
+		return arr[i];
+  	}
+  
+  }
+ 
+}
+
+ 
+ /*
+first we usr arr.flat to make your initial array 1 array.
+then we now find Math.max of your newArray and put max in its own variable.
+then we iterate through every array in your arr using a for loop
+then we now find if your max number is in any of the arrays so arr[i].indexOf(maxVariable)
+If it exist in that array, return that array(arr[i]);
+ */
+
+
+
+function largestOfFour1(arr) {
+  // You can do this!
+  let maxArr = [];
+  for(let i=0; i<arr.length; i++){
+  	maxArr.push(Math.max(...arr[i]));
+  	}
+  return maxArr;
+  }
+ 
+//This returns all max numbers in 1 array
+
+
+
+
+
+
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+  let ending = str.substring(str.length-target.length, str.length);
+  if(ending === target){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+ confirmEnding("Bastian", "n");
+
+//or
+
+function confirmEnding1(str, target){
+  let check = str.slice(str.length -target.length) === target;
+  return check;
+}
+
+  confirmEnding1("Bastian", "n");
+
+
+
+
+//checks if the end of a string marches a target and returns true or false
+
+
+
+
+
+
+
+//Repeat a String Repeat a String
+
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  if(num<=0){
+    return '';
+  }else{
+    return str + repeatStringNumTimes(str, num-1);
+  }
+}
+
+//or
+
+function repeatStringNumTimes1(str, num){
+  let repeat='';
+  for(let i=0; i<num; i++){
+      repeat += str;
+    
+  }
+  return repeat;
+}
+
+/*
+i would create a for loop to iterate from 0 to num 
+then i will check if num is <= 0 the return blank space
+else r
+*/
+
+
+
+//truncate a string
+function truncateString(str, num) {
+  // Clear out that junk in your trunk
+  return str.slice(0,num) + '...';
+}
---
+++ RecursionPractice/recursionPractice.js
@@ -0,0 +1,117 @@
+// const factorial = function(num){
+// 	if(num <= 1) {
+// 		return 1;
+// 	}else{
+// 		return num * factorial(num-1);
+// 	}
+// }		
+
+// factorial(4) (6*4) = 24
+// factorial(4);
+//          4 * factorial(4-1) = 3 
+// factorial(3);
+// 		3 * factorial(3-1)= 2  6
+// factorial(2);
+// 		2 * factorial(2-1)= 1 2
+// factorial(1);
+// 		return 1
+
+
+
+
+//what is a fibinocci sequence
+//	-a seriece of number where the next number is found by adding the previous 2 numbers
+
+/*
+
+Problem statement
+write a function the returns the fibonocci sequence for a value of indexes
+for example
+fibonocci(6) //should output : [0, 1, 1, 2, 3, 5, 8]
+
+0 1 1 2 3 5
+ fibinocci (num)
+ next number = previous number + 2nd previous
+               (num-1) + (num-2)
+
+
+Method for solving problems 
+
+1. Define what you know about the problem
+
+2. O-ICE
+
+	O - Output: define what your function output
+
+	I - Input: define what your function will input
+
+	C - Constraints: anything that creates boundaries for the problem
+
+	E - Edge Cases : e.g. the sequence can only be made with positive numbers 
+
+	--------base case ==C-Constraints-In Fib-num has to be < 1 if numb is 1 or less return mum and 
+									  end loop
+	--------recursive case==c-Constraint- as long as num > 1 keep running recursive case
+						    (num-2) ,(num-1). this gives you the previous 2 numbers
+	--------input is 5
+	--------output should be 5,3,2,1,1,0 == 0,1,1,2,3,5
+	--I create a function that takes a number as a parameter
+	--Write the base case
+		-tests your fariable to see if it is true. if it is turn then it stops the function
+		-if it is false is moves on to the recursive case
+		-the base case here is "if(num<=1){return num}"
+	create a variable to hold the value in the fibanocci sequence
+	--write the recursive case
+		-else{ return fibanocci(num-2) + fibanocci(num-1)}
+
+
+
+
+Fibonacci
+let fibonacci =>(num){
+
+}
+
+
+fibonacci(5);
+
+
+
+
+reversal string
+function that will return a rverse of a given string
+
+
+
+
+Recursive arrays
+flatten([1, 2, 3, 4])
+let g = [1, [2], [3, [[4]]]];
+g.flat(3)
+-what .flat() does is it takes an array with multiple dimention and make it flat or one dimention. 
+-And if you have multiple dimentions in your array you would count the different levels of nesting
+ then use that number as a parameter in your .flat() built in function. for instance take this array
+ [1, [2], [3, [[4]]]] , the 4 is inside of an array1 and that array is inside another array2 and 
+ that array is inside another array3 that has 3 also inside of it and as you can see that is the the 
+ base dimention where 1 is and 2 is. but you see that 2 is in 1 array and there is no other array 
+ inside of it. so since the highest number of nesting is 3, then you pass the 3 into the .flat().
+ so .flat(3);
+
+flatten([1, 2, 3, 4])
+
+function flatten(arr) {
+  var result = []
+  arr.forEach(function(element) {
+  		
+      if (!Array.isArray(element)) {
+            result.push(element)    
+      } else {
+            result = result.concat(flatten(element))    
+      }  
+  })  
+  return result
+}
+
+*/
+
+
