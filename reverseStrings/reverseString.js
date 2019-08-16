c171545da7323ae127e1e89effb6803a4283456e
Sema <Danteh.Sema@baltimorecity.gov>
2019-08-15 21:27:49 -0400

    reverseString

--- reverseStrings/reverseString.js
+++ reverseStrings/reverseString.js
@@ -11,3 +11,23 @@ in order to make your function run
 
 */
 
+/*
+list of steps
+1 i need to split each character in the string
+then i need to reverse the array
+then i will now join the array back to one string.
+*/
+
+function reverseString(str){
+	return str.split('').reverse().join('');
+}
+
+function reverseString2(str){
+	let stringSplit = [];
+	let stringReverse = [];
+	stringSplit = str.split('');
+	for(let i=stringSplit.length-1; i>=0; i--){
+		stringReverse.push(stringSplit[i]);
+	}
+	return stringReverse.join('');
+}
