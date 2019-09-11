9c92242abf97c59e566b81aacbdf6c27d576ece6
Sema <dantehsema@yahoo.com>
2019-09-11 18:01:25 -0400

    classicPrime

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -9,6 +9,17 @@ var primeTester = function(n) {
   if(typeof n !== 'number' || n < 1 || n % 1 !== 0){
     // n isn't a number or n is less than 1 or n is not an integer
     return false;
+  } else if(n===2 && n===3 && n===5 && n%2 !== 0 && n%3 !==0 && n%5 !== 0){
+  	return true;
   }
+  
   // TODO: return true if n is prime, false otherwise
 };
+
+/*else if (typeof n === 'number' && n/1 === n && n/n === 1 && n ===1 && n === 2 && n === 3 && n%2 !== 0 && n%3 !== 0 && n%5 !== 0 ){
+  	console.log('this is value of n/1 and n/n', n/n)
+  	return true;
+else if (typeof n === 'number' && n/1 === n && n/n === 1 && n ===1 && n === 2 && n === 3 && n%2 !== 0 && n%3 !== 0 && n%5 !== 0 ){
+  	console.log('this is value of n/1 and n/n', n/n)
+  	return true;
+*/
