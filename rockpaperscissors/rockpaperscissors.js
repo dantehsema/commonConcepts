0270a88720075b0addc5a2d10bfa9437c3e8f823
Sema <dantehsema@yahoo.com>
2019-09-15 15:01:58 -0400

    rock

--- rockpaperscissors/rockpaperscissors.js
+++ rockpaperscissors/rockpaperscissors.js
@@ -11,6 +11,57 @@
 //      ]
 
 
+
+
+/*
+I need an array that hold the possible throws in a game of Rock Paper Scissors
+We need an array to hold the current plays
+we need an array to hold the outcomes
+we need a variable to hold the current round
+
+The base case
+we need to iterate through the possible throws and push them to the current plays array
+do this until rounds = 0 then push current plays array into the outcomes array
+*/
+
+let rounds = 3;
+let outcomes = [];
+let plays=['rock', 'paper', 'scissors'];
+
+
+games = (rounds, playedSoFar) => {
+	
+	if(rounds === 0){
+		outcomes.push(playedSoFar);
+		return;
+	}
+	for(let i=0; i<plays.length; i++){
+	let currentPlay = plays[i];
+	games(rounds-1, playedSoFar.concat(currentPlay));
+}
+
+return outcomes;
+}
+
+let c = games(rounds,[]);
+
+/*
+round 1
+
+	games 1
+		playedsofar [rock]
+	games 2
+		playedSoFar [rock, rock]
+	games 3
+		playedSoFar [rock, rock, rock]
+game 2
+	round 1
+		playedsofar [pa]
+*/
+
+
+
+
 // ************Extra Credit***************
 
 // Make your function return answers for any number of rounds
