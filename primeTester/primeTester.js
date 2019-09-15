64a2eeab2632e400347bdeb37f32bccd3fb215d9
Sema <dantehsema@yahoo.com>
2019-09-15 12:46:57 -0400

    primeTester

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -9,10 +9,18 @@ var primeTester = function(n) {
   if(typeof n !== 'number' || n < 1 || n % 1 !== 0){
     // n isn't a number or n is less than 1 or n is not an integer
     return false;
-  } else if(n===2 && n===3 && n===5 && n%2 !== 0 && n%3 !==0 && n%5 !== 0){
-  	return true;
-  }
   
+  }
+  console.log('this is n-1:', n-1);
+  for(let i=2; i<n-1; i++){
+        console.log('this is i', i);
+    if(n%i===0){
+        console.log('divisible by i, n', i, n);
+      return false;
+    }
+
+  }
+    return true;
   // TODO: return true if n is prime, false otherwise
 };
 
